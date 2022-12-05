88c5a2ed4345002cc0c561ae598a4beff7e5bc9d
Philipp Sieweck <psi@informatik.uni-kiel.de>
2022-12-05 14:23:57 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod app;
+
+use app::TemplateApp;
+
+fn main() {
+    // Make sure panics are logged using `console.error`.
+    console_error_panic_hook::set_once();
+
+    // Redirect tracing to console.log and friends:
+    tracing_wasm::set_as_global_default();
+
+    let web_options = eframe::WebOptions::default();
+    eframe::start_web(
+        "the_canvas_id", // hardcode it
+        web_options,
+        Box::new(|cc| Box::new(TemplateApp::new(cc))),
+    )
+    .expect("failed to start eframe");
+}
